47aff309b1b5ba86f2addf06e9b528d66705c391
pld <peter@helioid.com>
2014-04-09 14:12:55 +0300

    PLD/RR: add test lib and tests for home-page

--- project.clj
+++ project.clj
@@ -7,5 +7,7 @@
                  [hiccup "1.0.0"]
                  [http-kit "2.1.12"]
                  [ring.middleware.logger "0.4.0"]]
-  :plugins [[lein-ring "0.7.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :plugins [[lein-ring "0.7.1"]
+            [lein-midje "3.1.3"]]
   :ring {:handler ona.viewer.routes/app})
---
+++ test/ona/viewer/views/home_test.clj
@@ -0,0 +1,13 @@
+(ns ona.viewer.views.home-test
+  (:use midje.sweet
+        ona.viewer.views.home))
+
+(facts "about home-page"
+       "Home page goes to sign in if no session"
+       (home-page {}) => :sign-in
+       (provided (sign-in) => :sign-in)
+
+       "Home page goes to dashboard if account in session"
+       (let [fake-account :fake-account]
+         (home-page {:account fake-account}) => :dashboard
+         (provided (dashboard fake-account) => :dashboard)))
