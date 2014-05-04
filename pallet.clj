22610fa6f5eb76e2925e64ca0c015842f3264046
pld <peter@helioid.com>
2014-05-04 10:55:18 -0400

    PLD: move spec to def, add runit user

--- pallet.clj
+++ pallet.clj
@@ -1,9 +1,19 @@
-(require '[pallet.crate.java :as java]
+(require '[pallet.api :refer [make-user plan-fn]]
+         '[pallet.crate.automated-admin-user :as automated-admin-user]
+         '[pallet.crate.java :as java]
          '[pallet.crate.runit :as runit]
          '[pallet.crate.app-deploy :as app-deploy])
 
-(def webserver
-  (group-spec "webserver"
+(def ona-viewer-node-spec
+  {:image {:os-family :ubuntu
+           :os-version-matches "12.04"
+           :os-64-bit true
+           :image-id "us-east-1/ami-e2861d8b"}
+   :location {:location-id "us-east-1a"}
+   :network {:inbound-ports [22 8080]}})
+
+(def ona-viewer-server
+  (group-spec "ona-viewer-server"
               :extends [(java/server-spec {})
                         (runit/server-spec {})
                         (app-deploy/server-spec
@@ -16,11 +26,8 @@
 
 (defproject ona-viewer
   :provider {:aws-ec2
-             {:node-spec
-              {:image {:os-family :ubuntu
-                       :os-version-matches "12.04"
-                       :os-64-bit true
-                       :image-id "us-east-1/ami-e2861d8b"}
-               :location {:location-id "us-east-1a"}
-               :network {:inbound-ports [22 8080]}}}}
-  :groups [webserver])
+             {:node-spec ona-viewer-node-spec
+              :phases {:bootstrap
+                       (plan-fn (automated-admin-user)
+                                (make-user "runit"))}}}
+  :groups [ona-viewer-server])
