f7f86fd7ab638c5a4b73f494ddd3c9bbf382a83c
royrutto <royrutto@gmail.com>
2014-05-05 18:15:21 +0300

    RR: add navigation items

--- src/clj/ona/viewer/views/templates.clj
+++ src/clj/ona/viewer/views/templates.clj
@@ -2,10 +2,24 @@
   (:use [hiccup core page])
   (:require [net.cgrand.enlive-html :as html]))
 
+(def navigation-items
+  {"Home" "/"
+   "Project" "/projects"
+   "Organizations" "/organization"
+   "Sign-up" "/sign-up"
+   "Sign-out" "/signout"
+   })
+
 (html/deftemplate base-template "templates/base.html"
-  [title page-content]
+  [{:keys [current-path]} title page-content]
   [:head :title] (html/content title)
   [:body :h1.title] (html/content title)
+  [:ul.nav [:li html/first-of-type]] (html/clone-for [[caption url] navigation-items]
+                                                     [:li] (if (= current-path url)
+                                                             (html/set-attr :class "active")
+                                                             identity)
+                                                     [:li :a] (html/content caption)
+                                                     [:li :a] (html/set-attr :href url))
   [:body :div.content] (html/append page-content))
 
 (html/defsnippet signin-form "templates/sign-in.html"
@@ -13,5 +27,5 @@
   [])
 
 (defn sign-in-form
-  []
-  (base-template "Sign-in" (signin-form)))
+  [request]
+  (base-template request "Sign-in" (signin-form)))
