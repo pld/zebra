954b8fc8a8c17365e206f5386cf82eeeb12fec75
pld <peter@helioid.com>
2014-05-05 22:21:25 -0400

    PLD: add port 80 to default open on deploy

--- pallet.clj
+++ pallet.clj
@@ -11,7 +11,7 @@
            :os-64-bit true
            :image-id "us-east-1/ami-e2861d8b"}
    :location {:location-id "us-east-1a"}
-   :network {:inbound-ports [22 8080]}})
+   :network {:inbound-ports [22 80 8080]}})
 
 (def ona-viewer-server
   "Group spec with app deploy paths for Ona viewer."
