d5a4e1388d6c0a26628fb913c7799f30058c67ca
royrutto <royrutto@gmail.com>
2014-05-08 11:15:15 +0300

    RR: add api get tag function

--- src/clj/ona/api/dataset.clj
+++ src/clj/ona/api/dataset.clj
@@ -16,3 +16,7 @@
 (defn submission [account dataset-id submission-id]
   (let [url (make-url "data/" (:username account) "/" dataset-id "/" submission-id)]
     (parse-http :get url account)))
+(defn tags [account dataset-id]
+  (let [url (make-url "forms/" (:username account) "/" dataset-id "/" "labels")]
+    (parse-http :get url account)
+    ))
