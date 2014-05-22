a314beb1b68a3bb49938cd4e215ed6423baa420c
royrutto <royrutto@gmail.com>
2014-05-22 16:07:40 +0300

    RR: show tags for dataset

--- src/clj/ona/viewer/templates/datasets.clj
+++ src/clj/ona/viewer/templates/datasets.clj
@@ -20,6 +20,11 @@
   [:body :div#content]
   [metadata dataset]
 
+  ;;Page-title
   [:div.page-header [:div first-of-type] :h1](content (:title metadata))
+
+  ;;Sidenav
   [:div#sidenav [:p (nth-of-type 2)]](content (:description metadata))
-  )
+  [:p.tagbox [:span.tag (but first-of-type)]] nil
+  [:p.tagbox [:span.tag first-of-type]](clone-for [tag (:tags metadata)]
+                                                               [:span.tag] (content tag)))
