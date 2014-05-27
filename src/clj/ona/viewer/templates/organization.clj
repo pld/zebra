645d160c77212437dda338763666464bb01e7988
pld <peter@helioid.com>
2014-05-27 16:18:45 -0400

    PLD: add do-> statements to org template helper

--- src/clj/ona/viewer/templates/organization.clj
+++ src/clj/ona/viewer/templates/organization.clj
@@ -2,6 +2,7 @@
   (:use [net.cgrand.enlive-html :only [but
                                        clone-for
                                        content
+                                       do->
                                        first-of-type
                                        defsnippet
                                        set-attr]] :reload))
@@ -14,17 +15,17 @@
   [:div.org-details [:h4 first-of-type]] (content (:name (:org org-details)))
   [:div.org-details :> :span.city] (content (:city (:org org-details)))
   [:div.org-details :> :span.country] (content (:country (:org org-details)))
-  [:div.org-details :a.org-url] (content (:url (:org org-details)))
-  [:div.org-details :a.org-url] (set-attr :href (:url (:org org-details)))
+  [:div.org-details :a.org-url] (do-> (content (:url (:org org-details)))
+                                      (set-attr :href (:url (:org org-details))))
   ;; Set Member details
-  [:div.org-details :a.members] (content (str "Members (" (count(:members org-details)) ")"))
-  [:div.org-details :a.members] (set-attr :href (str  "/organizations/" (:org (:org org-details)) "/members"))
+  [:div.org-details :a.members] (do-> (content (str "Members (" (count(:members org-details)) ")"))
+                                      (set-attr :href (str  "/organizations/" (:org (:org org-details)) "/members")))
   [:div.org-details :ul.members [:li (but first-of-type)]] nil
   [:div.org-details :ul.members [:li first-of-type]](clone-for [team (:members org-details)]
                                                                   [:li] (content team))
   ;; Set Team details
-  [:div.org-details :a.teams] (content (str "Teams (" (count(:teams org-details)) ")"))
-  [:div.org-details :a.teams] (set-attr :href (str  "/organizations/" (:org (:org org-details)) "/teams"))
+  [:div.org-details :a.teams] (do-> (content (str "Teams (" (count(:teams org-details)) ")"))
+                                    (set-attr :href (str  "/organizations/" (:org (:org org-details)) "/teams")))
   [:div.org-details :ul.teams [:li (but first-of-type)]] nil
   [:div.org-details :ul.teams [:li first-of-type]](clone-for [team (:teams org-details)]
                                                                [:li] (content (:name team))))
@@ -33,7 +34,7 @@
 (defsnippet teams "templates/teams.html"
   [:body :div#content]
   [org teams]
-  [:div.myteams]nil
+  [:div.myteams] nil
   [:div.orgteams [:.orgteam (but first-of-type)]] nil
   [:div.orgteams :.orgteam] (clone-for [team teams]
                            [:h3](content (:name team)))
@@ -43,8 +44,8 @@
 (defsnippet new-team "templates/new-team.html"
   [:body :div#content]
   [org]
-  [:form#new-team](set-attr :action (str "/organizations/" (:org org) "/new-team"))
-  [:form#new-team](set-attr :method "post")
+  [:form#new-team] (do-> (set-attr :action (str "/organizations/" (:org org) "/new-team"))
+                         (set-attr :method "post"))
   [:form#new-team  :input#org](set-attr :value (:org org)))
 
 (defsnippet members "templates/members.html"
