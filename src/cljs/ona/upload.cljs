7e554bb54b478369149bb7b9e6cd87112449343d
pld <peter@helioid.com>
2014-06-03 17:54:26 -0400

    PLD: remove errant alert

--- src/cljs/ona/upload.cljs
+++ src/cljs/ona/upload.cljs
@@ -34,7 +34,6 @@
   "Check if an error was returned by "
   [io]
   (try
-    (js/alert (.getResponseText io))
     (not (aget (JSON/parse (.getResponseText io)) "settings-url"))
     (catch js/SyntaxError e
       true)))
