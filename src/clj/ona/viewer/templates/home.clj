79632b19933dbebc4f779ce325ad9a1a69d8edb5
royrutto <royrutto@gmail.com>
2014-06-04 12:03:05 +0300

    RR: WIP count public/private datasets

--- src/clj/ona/viewer/templates/home.clj
+++ src/clj/ona/viewer/templates/home.clj
@@ -12,9 +12,14 @@
 
 (defsnippet home-content "templates/home.html"
   [:body :div#content]
-  [username datasets]
+  [username datasets dataset-details]
   [:.username] (content username)
   [:div.datasets-table] (content (datasets/datasets-table datasets username))
 
   ;; Set sidenav links
-  [:#sidenav [:a first-of-type]] (set-attr :href "/project"))
+  [:#sidenav [:a first-of-type]] (set-attr :href "/project")
+
+  ;; Dataset details
+  [:span#public-datasets] (content (str (:no-of-public dataset-details)))
+  [:span#private-datasets] (content (str (:no-of-private dataset-details))))
+
