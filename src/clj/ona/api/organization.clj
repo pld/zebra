c2bddce6541f593aa03678fd17fa74e1b752e34e
pld <peter@helioid.com>
2014-06-16 17:18:32 -0400

    PLD: move single owners to end after user funcs defined

--- src/clj/ona/api/organization.clj
+++ src/clj/ona/api/organization.clj
@@ -5,16 +5,6 @@
 
 (def owners-team-name "Owners")
 
-(defn single-owner?
-  "Is the user the only member of the Owners team."
-  ([team members]
-     (and (= owners-team-name (-> team :name))
-          (= 1 (count members))))
-  ([account org-name team-id]
-     (single-owner?
-      (team-info account org-name team-id)
-      (team-members account org-name team-id))))
-
 (defn all [account]
   (let [url (make-url "orgs")]
     (parse-http :get url account)))
@@ -78,3 +68,13 @@
               (make-url "teams" org-name team-id "members")
               (make-url "orgs" org-name "members"))]
     (parse-http :delete url account {:query-params {:username member}})))
+
+(defn single-owner?
+  "Is the user the only member of the Owners team."
+  ([team members]
+     (and (= owners-team-name (-> team :name))
+          (= 1 (count members))))
+  ([account org-name team-id]
+     (single-owner?
+      (team-info account org-name team-id)
+      (team-members account org-name team-id))))
