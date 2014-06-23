e331281d793235e7487de65e8d2415c87c3364cd
royrutto <royrutto@gmail.com>
2014-06-23 16:15:37 +0300

    RR: private function for field-name query field

--- src/clj/ona/api/charts.clj
+++ src/clj/ona/api/charts.clj
@@ -1,14 +1,20 @@
 (ns ona.api.charts
   (:use [ona.api.io :only [make-url parse-http]]))
 
+(defn- ext
+  ([dataset-id]
+   (str dataset-id ".json"))
+  ([dataset-id field-name]
+   (str dataset-id ".json?field_name=" field-name)))
+
 (defn fields
   "Get list of chart fields for a specific dataset"
   [account dataset-id]
-  (let [url (make-url "charts" (str dataset-id ".json"))]
+  (let [url (make-url "charts" (ext dataset-id))]
         (parse-http :get url account)))
 
 (defn chart
   "Get chart for a specifc field in a dataset"
   [account dataset-id field-name]
-  (let [url (make-url "charts" (str dataset-id ".json?field_name=" field-name))]
+  (let [url (make-url "charts" (ext dataset-id field-name))]
     (parse-http :get url account)))
