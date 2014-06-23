f209bca93896d816967e155187b4314f440961d8
royrutto <royrutto@gmail.com>
2014-06-23 15:56:33 +0300

    RR: fix error for empty maps (charts without data)

--- src/clj/ona/utils/charts.clj
+++ src/clj/ona/utils/charts.clj
@@ -16,15 +16,17 @@
         bar-height 20
         field_xpath (:field_xpath chart-data)
         data (:data chart-data)
-        extracted-data (apply merge (for [data-item data]
-                      {((keyword field_xpath) data-item) (:count data-item)}))
-        s (scale/linear :domain [0 (apply max (vals extracted-data))]
-                        :range [0 width])]
-        [:rect.bars
-            (unify extracted-data (fn [[label val]]
-                          [:div (style :heigth (str bar-height "px")
-                                       :width (str (s val) "px")
-                                       :background-color "blue"
-                                       :opacity 0.6
-                                       :margin "2px")
-                           [:span (style :color "yellow") label]]))]))
+        extracted-data (into {} (for [data-item data]
+                                  [((keyword field_xpath) data-item)
+                                   (:count data-item)]))
+        s (if (> (count extracted-data) 0)
+            (scale/linear :domain [0 (apply max (vals extracted-data))]
+                          :range [0 width]))]
+    [:rect.bars
+     (unify extracted-data (fn [[label val]]
+                             [:div (style :heigth (str bar-height "px")
+                                          :width (str (s val) "px")
+                                          :background-color "blue"
+                                          :opacity 0.6
+                                          :margin "2px")
+                              [:span (style :color "yellow") label]]))]))
